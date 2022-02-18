c698ce0fca246e214f894b6bd97f4ae26d8ea5b7
Ruben Paredes <ruben.pardes25@gmail.com>
2022-02-18 13:39:52 -0400

    Implement arrange items according to div width

--- src/AnimatedCode.tsx
+++ src/AnimatedCode.tsx
@@ -1,35 +1,13 @@
 import React, { ReactNode, useEffect, useState } from "react";
-import { animated, easings, useSpring, useTransition } from "@react-spring/web";
 import {
   Light as SyntaxHighlighter,
   SyntaxHighlighterProps,
 } from "react-syntax-highlighter";
 import js from "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
-import docco from "react-syntax-highlighter/dist/esm/styles/hljs/docco";
 
 SyntaxHighlighter.registerLanguage("javascript", js);
 
-const FadeIn = ({ isVisible, children }: any) => {
-  const styles = useSpring({
-    opacity: isVisible ? 1 : 0,
-    y: isVisible ? 0 : 24,
-  });
-
-  return <animated.div style={styles}>{children}</animated.div>;
-};
-
-const getMoveRightStyles = () =>
-  useSpring({
-    from: {
-      x: 0,
-    },
-    to: {
-      x: window.screen.availWidth,
-    },
-    config: {
-      duration: 10000,
-    },
-  });
+const SPEED_CONSTANT = 35 / 1920; // ~ 0.01823
 
 /**
  * Returns only the components that are strings
@@ -81,81 +59,128 @@ const getCodeLines = (
   }
 };
 
+function getHowManyFitIn(base: HTMLElement, target: HTMLElement) {
+  const { width: targetWidth } = target.getBoundingClientRect();
+  return Math.ceil(targetWidth / base.offsetWidth);
+}
 export interface AnimatedCodeProps extends SyntaxHighlighterProps {
   children: ReactNode;
 }
 
+/**
+ * @todo
+ * - [X] [DONE] Make it work with any parent component
+ * - Have two rows moving simultaneously
+ * - Achieve infite loop
+ * - Move each row independently
+ * - Adjust when parent width changes
+ *
+ * - Use SyntaxHighlighter
+ * - [Maybe] change this component name to AnimatedText (or similiar)
+ * and make it only work in its most simpler way with plain text, but sufficiently
+ * customizable to be able to change the text renderer component so I can use SyntaxHighlighter.
+ * Then make a wrapper component with SyntaxHighlighter
+ *
+ */
 function AnimatedCode({
   children,
   ...syntaxHighlighterProps
 }: AnimatedCodeProps) {
   // console.log("🚀 ~ file: AnimatedCode.tsx ~ line 40 ~ children", children);
   const [isVisible, setIsVisible] = useState(false);
-  const individualTextElementsRefs = React.useRef<any[]>([]);
-  const rightmostRefs = React.useRef<any[]>([]);
+  /**
+   * Will store the main text refs of each row.\
+   * Later used to calculate how many will fit in the container
+   */
+  const individualTextElementsRefs = React.useRef<HTMLElement[]>([]);
+  const containerRef = React.useRef<HTMLDivElement>(null);
+
+  const rightmostRefs = React.useRef<HTMLDivElement[]>([]);
   const codeLines = getCodeLines(children);
-  const [elementsToFitWindow, setElementsToFitWindow] = useState<{
-    [f: number]: number;
-  }>({});
+  /**
+   * Will store the minimun amount of `individualTextElementsRefs` elements that
+   * each row has to have, to fullfil all the container width\
+   * Each index. corresponds to each row.
+   * @example `elementsToFitContainer[1] corresponds to the row 1 (one)`
+   */
+  const [elementsToFitContainer, setElementsToFitContainer] = useState<
+    number[]
+  >([]);
   // console.log("🚀 ~ file: AnimatedCode.tsx ~ line 48 ~ codeLines", codeLines);
 
+  const moveRightStyle = getMoveRightStyles(containerRef.current)
+
+  // TESTING
   useEffect(() => {
     console.log(
-      "🚀 ~ file: AnimatedCode.tsx ~ line 97 ~ elementsToFitWindow",
-      elementsToFitWindow
+      "🚀 ~ file: AnimatedCode.tsx ~ line 97 ~ elementsToFitContainer",
+      elementsToFitContainer
     );
-  }, [elementsToFitWindow]);
+  }, [elementsToFitContainer]);
 
   useEffect(() => {
-    individualTextElementsRefs.current.forEach((element) =>
-      console.log(
-        `Element text "${element.innerText}", width: ${element.offsetWidth}`
-      )
-    );
+    if (containerRef.current) {
+      const containerRect = containerRef.current.getBoundingClientRect();
 
-    const _elementsToFitWindow = individualTextElementsRefs.current.map(
-      (element) => {
-        return Math.ceil(window.innerWidth / element.offsetWidth);
-      }
-    );
-    setElementsToFitWindow(_elementsToFitWindow);
-    const intervalID = setInterval(() => {
-      if (!rightmostRefs.current[0].getBoundingClientRect) return
-      const { left } = rightmostRefs.current[0].getBoundingClientRect()
-      if (left >= window.innerWidth) {
-        console.log('The end has been reached')
-      }
-    }, 100)
-    return () => clearInterval(intervalID)
+      // START TESTING
+      individualTextElementsRefs.current.forEach((element) =>
+        console.log(
+          `Element text "${element.innerText}", width: ${element.offsetWidth}`
+        )
+      );
+      // END TESTING
+
+      const _elementsToFitContainer = individualTextElementsRefs.current.map(
+        (element) => {
+          if (!containerRef.current) return 0; // <- Dont know why TS gives error without this line
+          return getHowManyFitIn(element, containerRef.current);
+        }
+      );
+
+      setElementsToFitContainer(_elementsToFitContainer);
+
+      const intervalID = setInterval(() => {
+        if (!rightmostRefs.current[0].getBoundingClientRect) return;
+        const rightmostElementRect =
+          rightmostRefs.current[0].getBoundingClientRect();
+        if (rightmostElementRect.right > containerRect.width) {
+          // The
+        }
+        if (rightmostElementRect.left >= containerRect.width) {
+          // The last element is outside the container
+          // Steps
+          // 1. Remove it
+          // 2. Push a new element at first position (unshift)
+          console.log("The end has been reached");
+        }
+        //
+      }, 100);
+      return () => clearInterval(intervalID);
+    }
   }, []);
 
   const handleOnClick = () => {
     setIsVisible(!isVisible);
   };
+  // START TESTING
   const logRef = () =>
-    console.log(
-      "[AnimatedCode][logRef] ref",
-      individualTextElementsRefs
-    );
+    console.log("[AnimatedCode][logRef] ref", individualTextElementsRefs);
   const logRightmostRef = () =>
-    console.log(
-      "[AnimatedCode][logRightmostRef] rightmostRefs",
-      rightmostRefs
-    );
+    console.log("[AnimatedCode][logRightmostRef] rightmostRefs", rightmostRefs);
   const logRightmostRefBoundingRect = () =>
     console.log(
       "[AnimatedCode][logRightmostRef] rightmostRefs[0].getBoundingClientRect()",
       rightmostRefs.current[0].getBoundingClientRect()
     );
+  // END TESTING
   return (
     <div>
-      {/* <FadeIn isVisible={isVisible}>Hola</FadeIn> */}
-      <div style={{overflow: 'hidden'}}>
+      <div style={{ overflow: "hidden", width: '50%' }} ref={containerRef}>
         {codeLines?.map(({ text, ..._syntaxHighlighterProps }, i) => {
           console.log("[codeLines][map] i:", i);
           console.log(
-            "[codeLines][map] elementsToFitWindow[i]:",
-            elementsToFitWindow[i]
+            "[codeLines][map] elementsToFitContainer[i]:",
+            elementsToFitContainer[i]
           );
           const IndividualTextElement = React.createElement(
             "p",
@@ -173,14 +198,13 @@ function AnimatedCode({
             text
           );
           const totalTextElements = [];
-          if (elementsToFitWindow[i]) {
-            for (let j = 0; j < elementsToFitWindow[i] - 1; j++) {
+          if (elementsToFitContainer[i]) {
+            for (let j = 0; j < elementsToFitContainer[i] - 1; j++) {
               totalTextElements.push(
                 React.cloneElement(IndividualTextElement, { key: j })
               );
             }
           }
-          // console.log("🚀 ~ file: AnimatedCode.tsx ~ line 108 ~ {codeLines?.map ~ individualTextElement", IndividualTextElement)
           return (
             <div key={i}>
               <div
@@ -200,37 +224,25 @@ function AnimatedCode({
           );
         })}
       </div>
-      <div style={{ ...(isVisible && moveRightStyle) }}>Hola</div>
+      {/* START TESTING */}
       <button onClick={handleOnClick}>Change</button>
       <button onClick={logRef}>Log REF</button>
       <button onClick={logRightmostRef}>Log Rightmost REF</button>
-      <button onClick={logRightmostRefBoundingRect}>Log Rightmost REF getBoundingClientRect</button>
-      {/* {codeLines?.map(({ text, ..._syntaxHighlighterProps }, i) => (
-        <SyntaxHighlighter
-          key={i}
-          language={"javascript"}
-          style={docco}
-          customStyle={{
-            backgroundColor: "white",
-          }}
-          {...syntaxHighlighterProps}
-          {..._syntaxHighlighterProps}
-        >
-          {text}
-        </SyntaxHighlighter>
-      ))} */}
+      <button onClick={logRightmostRefBoundingRect}>
+        Log Rightmost REF getBoundingClientRect
+      </button>
+      {/* END TESTING */}
     </div>
   );
 }
 
 export default AnimatedCode;
 
-const moveRightStyle = {
-  transform: `translateX(${window.screen.availWidth}px)`,
-  transition: `transform ${(35 * window.screen.availWidth) / 1920}s linear`,
-};
-
-const moveLeftStyle = {
-  transform: `translateX(-${window.screen.availWidth}px)`,
-  transition: `transform ${(35 * window.screen.availWidth) / 1920}s linear`,
+const getMoveRightStyles = (container: HTMLElement | null) => {
+  if (!container) return {}
+  const { width: containerWidth } = container.getBoundingClientRect();
+  return {
+    transform: `translateX(${containerWidth}px)`,
+    transition: `transform ${SPEED_CONSTANT * containerWidth}s linear`,
+  };
 };
